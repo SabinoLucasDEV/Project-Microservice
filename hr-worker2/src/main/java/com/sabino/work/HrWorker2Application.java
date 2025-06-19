788c01c6c21b9b33367629e3760792f26c7e4cd8
N00bM4T3R <sabinolbn1810@gmail.com>
2025-06-19 11:23:21 -0300

    project created

---
+++ hr-worker2/src/main/java/com/sabino/work/HrWorker2Application.java
@@ -0,0 +1,13 @@
+package com.sabino.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorker2Application.class, args);
+	}
+
+}
---
+++ hr-worker2/src/test/java/com/sabino/work/HrWorker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabino.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
